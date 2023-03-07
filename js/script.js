27ec0db983c28e87db246bb10eae1f117631cf09
sollendy <edlannino@tutanota.com>
2023-03-07 17:05:35 +0100

    problemi con pianificazione degli if

--- js/script.js
+++ js/script.js
@@ -14,16 +14,23 @@ const numGenEl = document.getElementById('generatore');
 const numContEl = document.getElementById("numeri");
 
 //creo la variabile dell'array di numeri
-let randomNumbers = Array.from({length: 5}, () => Math.floor(Math.random() * 100) + 1);;
+let randomNumbers = Array.from({length: 5}, () => Math.floor(Math.random() * 10) + 1);;
 
 //in pagina spunteranno i numeri
 numContEl.innerHTML = `<b>${randomNumbers}</b>`;
 //console.log(randomNumbers);
 
-//dopo i numeri creo la funzione che genera il timer
-setTimeout (svanire, 10000);
+//dopo i numeri creo la funzione che genera i timer
+setTimeout (svanire, 3000);
+setTimeout (risposta, 3010);
+
 function svanire() {
     numContEl.style.display = "none";
-    let userNumbers = [prompt('quali erano i numeri?')];
+    //let userNumbers = [prompt('quali erano i numeri?')];
     //console.log(userNumbers);
 }
+
+function risposta() {
+    let userNumbers = [prompt('quali erano i numeri?')];
+}
+
