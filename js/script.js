0e06d3c535f7fc727b3e70811e3c1091937115f8
sollendy <edlannino@tutanota.com>
2023-03-07 15:14:27 +0100

    esercizio in corso

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//creo le variabili corrispondenti agli elementi html
+//creare variabile per visualizzare 5 numeri casuali nel dom
+//alla pressione del tasto verranno generati i numeri
+//assieme ai numeri genero un conto fino a 10 secondi alla fine del quale i numeri riportati spariranno
+//creo un prompt che chiede all'utente quali delle 5 cifre si ricorda
+//-->SE l'utente indovina anche solo una cifra gli riporto quale di esse sia
+//-->ALTRIMENTI gli dico che ha perso
+
+
+//creo le variabili corrispondenti agli elementi html
+const numGen = document.getElementById('generatore');
+const numContEl = document.getElementById("numeri");
